b201066816703e5cafcb3695e2b55b7cbd077781
Andre Sutherland <andrelsf95@gmail.com>
2025-12-10 16:46:51 +0000

    feat: expand styles to include elements used in the split pane tui

--- styles/styles.go
+++ styles/styles.go
@@ -15,6 +15,7 @@ type Styles struct {
 	ListItem          lipgloss.Style
 	Pagination        lipgloss.Style
 	Help              lipgloss.Style
+	Success           lipgloss.Style
 	Error             lipgloss.Style
 	Warning           lipgloss.Style
 	Info              lipgloss.Style
@@ -24,6 +25,13 @@ type Styles struct {
 	Location          lipgloss.Style
 	DiagnosticMessage lipgloss.Style
 	DiagnosticAction  lipgloss.Style
+	Header            lipgloss.Style
+	SelectedNavItem   lipgloss.Style
+	Hint              lipgloss.Style
+	Key               lipgloss.Style
+	// For displaying CLI commands (e.g. "bluelink deploy --name my-stack")
+	Command lipgloss.Style
+	Border  func(focused bool) lipgloss.Style
 
 	// Palette provides access to the underlying colors for custom rendering.
 	Palette ColorPalette
@@ -44,6 +52,7 @@ func NewStyles(r *lipgloss.Renderer, palette ColorPalette) *Styles {
 		ListItem:          r.NewStyle().PaddingLeft(4),
 		Pagination:        list.DefaultStyles().PaginationStyle.PaddingLeft(4),
 		Help:              list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1),
+		Success:           r.NewStyle().Foreground(palette.Success()),
 		Error:             r.NewStyle().Foreground(palette.Error()),
 		Warning:           r.NewStyle().Foreground(palette.Warning()),
 		Info:              r.NewStyle().Foreground(palette.Info()),
@@ -53,7 +62,28 @@ func NewStyles(r *lipgloss.Renderer, palette ColorPalette) *Styles {
 		Location:          r.NewStyle().MarginLeft(2).Foreground(palette.Primary()),
 		DiagnosticMessage: r.NewStyle().MarginLeft(2),
 		DiagnosticAction:  r.NewStyle().MarginTop(2),
+		Header:            r.NewStyle().Bold(true).Foreground(palette.Primary()),
+		SelectedNavItem:   r.NewStyle().Background(palette.Primary()).Foreground(palette.Text()),
+		Hint:              r.NewStyle().Foreground(palette.Primary()).Italic(true),
+		Key:               r.NewStyle().Foreground(palette.Primary()).Bold(true),
+		Command:           r.NewStyle().Foreground(palette.Info()),
+		Border:            borderStyleFunc(palette),
 		Palette:           palette,
+	}
+}
+
+func borderStyleFunc(palette ColorPalette) func(focused bool) lipgloss.Style {
+	return func(focused bool) lipgloss.Style {
+		focusedBorderColor := palette.Primary()
+		unfocusedBorderColor := palette.Muted()
+
+		baseStyle := lipgloss.NewStyle().BorderStyle(lipgloss.RoundedBorder())
+
+		if focused {
+			return baseStyle.BorderForeground(focusedBorderColor)
+		}
+
+		return baseStyle.BorderForeground(unfocusedBorderColor)
 	}
 }
 
